994c6b201ba44edfc8d799725c9fd5e75d74ea40
Mithali Anand <mithalianand@gmail.com>
2020-12-29 19:21:03 +0530

    added rank of player

--- js/Game.js
+++ js/Game.js
@@ -44,6 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+    Player.getCarsAtEnd();
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -92,7 +93,9 @@ class Game {
     }
 
     if(player.distance > 3860){
+      player.rank = player.rank + 1;
       gameState = 2;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +103,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank =  null;
   }
 
   getCount(){
@@ -30,6 +31,21 @@ class Player {
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
+    })
+  }
+
+
+  static getCarsAtEnd(){
+    var carsAtEndRef = database.ref('carsAtEnd');
+    carsAtEndRef.on("value",(data)=>{
+      carsAtEnd = data.val();
+    })
+  }
+
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd : rank
     })
   }
 }
